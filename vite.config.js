1303788bf199b09be9a1aae8510a7f99c3eacb2d
krosslerfrancisco <krosslerfrancisco@gmail.com>
2024-08-08 01:40:31 +0000

    ss

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://krossler.github.io/landingpage/',
+  base: 'https://krossler.github.io/landingpage',
 })
